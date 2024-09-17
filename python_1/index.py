7a65327082cbe47a60375140b807241778665610
Emanuel RIcardo <emanuelconde2012@gmail.com>
2024-09-17 20:33:54 -0300

    feat: Exercicios de If else

---
+++ python_1/index.py
@@ -0,0 +1,4 @@
+if 2 < 5:
+    print("Verdade")
+else:
+    print ("Mentira")
---
+++ python_2/index.py
@@ -0,0 +1,8 @@
+idade = 15
+if idade < 18:
+    print(f"Você tem {idade} anos, portanto, é menor de idade")
+elif idade >= 18 and idade < 65:
+    print(f"Por ter {idade} anos, você já é um adulto")
+else: 
+    print (f"Você é um idoso, pois já tem {idade} anos")
+
